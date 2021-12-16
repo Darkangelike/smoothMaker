0025273d877a56b99cbb226615686db97d20b572
Isabelle <isabelle.tieu@gmail.com>
2021-12-16 02:06:43 +0100

    missing user not existing

--- verification/index.php
+++ verification/index.php
@@ -10,6 +10,11 @@ if (isset($_GET["password"])) {
     $input_password = $_GET["password"];
 };
 
+$input_login = false;
+if (isset($_GET["login"])) {
+  $input_login = true;
+}
+
 
 $users = [
     [ "username" => "Marie",
@@ -23,21 +28,51 @@ $users = [
 ],
     [ "username" => "Ruben",
     "password" => "Trône"
-],
-
+], 
+    [ "username" => "Bagaudin",
+    "password" => "Absent"
+], 
+    [ "username" => "Abdel",
+    "password" => "Papa"
+], 
+    [ "username" => "Amina",
+    "password" => "Covid"
+], 
+    [ "username" => "Hedi",
+    "password" => "Araignée"
+], 
+    [ "username" => "Gislain",
+    "password" => "BN"
+], 
+    [ "username" => "Farid",
+    "password" => "Questions"
+], 
+    [ "username" => "Thomas",
+    "password" => "Blagues"
+], 
+    [ "username" => "Souleyman",
+    "password" => "Basketball"
+], 
+    [ "username" => "Karim",
+    "password" => "Rihanna"
+], 
+    [ "username" => "Pierre",
+    "password" => "Bon"
+], 
+    [ "username" => "Pierre",
+    "password" => "Bon"
+]
 ];
 
-$errorMessage = '<h2>This is an error message</h2>';
-
-$title = '<div class="text-center mt-3"><h1>Please sign in :</h1></div>';
+$title = '<div class="mt-3"><h1>Please sign in :</h1></div>';
 $webContent = '
-<form class="text-center mt-5">
+<form class="text-center mt-2">
     <ul>
         <li><label for="username">Username</label></li>
         <input id="username" name="username"></input>
         <li><label for="password">Password</label></li>
         <input id="password" class="mb-3" name="password" ></input>
-        <li><button class="btn btn-dark" type="submit">Submit</button></li>
+        <li><button class="btn btn-dark" name="login" type="submit">Submit</button></li>
     
 <li class="mt-3"><button class="btn btn-info" name="hint0" value="true">Hint 1</button>
 <button class="btn btn-info" name="hint1" value="true">Hint 2</button></li>
@@ -47,15 +82,76 @@ $webContent = '
 $hint0 = '<p class="text-center">Username:<br>Sainte dans la classe.<br>Password :<br>Un de ses jeux vidéos prérféré.</p>';
 $hint1 = '<p class="text-center">Username:<br>Le plus jeune de la classe.<br>Password:<br>Un de ses derniers achat. Version GoT.</p>';
 
+$errorMessage = false;
+if ($input_login) {
 foreach ($users as $user) {
 
-if ($user["username"] == $input_username && $user["password"] == $input_password) {
-$webContent = '<div class="text-center mt-3">
-<h1>You are the boss</h1>
-<img src="https://external-preview.redd.it/KFhkmiKGL90-tbfk6xvJghTqD1LCn_zMmrC1OBleEAo.jpg?width=640&crop=smart&auto=webp&s=fe63c4b5b3ef48c75d24c7dc683311908b18b8f7">
-</div>';
+if (!$input_username && !$input_password) {
+    $errorMessage = "<p>Please fill in the informations.</p>";
+  } else if ( !$input_username ) {
+    $errorMessage = "<p>Please input your username.</p>";
+  } else if (!$input_password) {
+    $errorMessage =  "<p>Please input your password.</p>";
+  } else if ($user["username"] == $input_username && $user["password"] != $input_password) {  
+   $errorMessage = "The password is incorrect.";
+  } else if ($user["username"] == $input_username && $user["password"] == $input_password) {
+      $webContent = '<div class="text-center mt-3">
+      <h1>You are the boss</h1>
+      <img src="https://external-preview.redd.it/KFhkmiKGL90-tbfk6xvJghTqD1LCn_zMmrC1OBleEAo.jpg?width=640&crop=smart&auto=webp&s=fe63c4b5b3ef48c75d24c7dc683311908b18b8f7">
+      </div>';
+      $errorMessage = false;
+      $title = false;
+} 
+
+
+
 }
+
+$title .= $errorMessage;
 }
+
+
+// if (!$input_username && !$input_password) {
+//     $errorMessage = "<p>Please fill in the informations.</p>";
+//   } else if (!$input_password) {
+//     $errorMessage =  "<p>Please input your password.</p>";
+//   } else if ( !$input_username ) {
+//     $errorMessage = "<p>Please input your username.</p>";
+//   } else if ( $user["username"] == $input_username && $user["password"] != $input_password ) {
+//   $errorMessage =  "<p>Incorrect password.</p>";
+//   } else if ($user["username"] != $input_username && $user["password"] != $input_password ) {
+//   $errorMessage =  "<p>This user does not exist.</p>";
+// } else if ($user["username"] == $input_username && $user["password"] == $input_password) {
+// $webContent = '<div class="text-center mt-3">
+// <h1>You are the boss</h1>
+// <img src="https://external-preview.redd.it/KFhkmiKGL90-tbfk6xvJghTqD1LCn_zMmrC1OBleEAo.jpg?width=640&crop=smart&auto=webp&s=fe63c4b5b3ef48c75d24c7dc683311908b18b8f7">
+// </div>';
+// $errorMessage = false;
+// }
+// }
+// } else {
+//   $errorMessage = false;
+// }
+
+
+// if ($user["username"] == $input_username && $user["password"] == $input_password) {
+// $webContent = '<div class="text-center mt-3">
+// <h1>You are the boss</h1>
+// <img src="https://external-preview.redd.it/KFhkmiKGL90-tbfk6xvJghTqD1LCn_zMmrC1OBleEAo.jpg?width=640&crop=smart&auto=webp&s=fe63c4b5b3ef48c75d24c7dc683311908b18b8f7">
+// </div>';
+// } ;
+// if ( $user["username"] == $input_username && $user["password"] != $input_password ) {
+//   $errorMessage = "Please input a valid password.";
+// } else if ( $user["username"] != $input_username && $user["password"] == $input_password) {
+//   $errorMessage = "Please input a valid username.";
+//   } else if ( !$input_username && !$input_password) {
+//     $errorMessage ="Please fill the informations.";
+//   } else if (!$input_password) {
+//     $errorMessage = "Please input your password.";
+//   } else {
+//     $errorMessage = "Please input your username.";
+//   }
+// }
 
 
 ?>
@@ -104,8 +200,10 @@ $webContent = '<div class="text-center mt-3">
 </nav>
 </header>
 
-<main>
+<main class="text-center container">
 <?php
+echo $input_username, $input_password;
+echo $title;
 echo $webContent ;
 
 if (isset ($_GET["hint0"])) {
